f3d4d9dea69c8334a55b0018d0de91508d09ce3d
Ekaterina Bauer <bauer-katerina@mail.ru>
2019-08-30 18:52:05 +0300

    Задача 3 работает!

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,28 +1,28 @@
 let dead = document.getElementById("dead");
 let lost = document.getElementById("lost");
 
-function getHole(index) {
-	let hole = document.getElementById(`hole${index}`);	
-	return hole;
-}
 
-for (let index = 1; index < 9; index++) {
-	getHole(index);
+		
+
+for (let index = 1; index <= 9; index++) {
+
+	let hole = document.getElementById(`hole${index}`);
+
 	hole.onclick = function() {
 		if (hole.className.includes("hole_has-mole") === true) {
-			dead++;
+			dead.textContent++;
 		} else {
-			lost++;
+			lost.textContent++;
 		}
 
-		if (dead.textContent === 10) {
+		if (dead.textContent == 10) {
 			alert ("Вы победили!");
-			dead = 0;
-			lost = 0;
-		} else if (lost.textContent === 5) {
+			dead.textContent = 0;
+			lost.textContent = 0;
+		} else if (lost.textContent == 5) {
 			alert ("Вы проиграли");
-			dead = 0;
-			lost = 0;
+			dead.textContent = 0;
+			lost.textContent = 0;
 			}
 	}
 }
