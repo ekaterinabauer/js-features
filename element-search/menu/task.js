492852e2075125571674bd529812eff25437ed6a
Ekaterina Bauer <bauer-katerina@mail.ru>
2019-09-01 21:10:00 +0300

    Задача 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,6 +1,11 @@
-let clicker = document.querySelectorAll('.menu__link');
-clicker.onclick = function() {
-	for (i = 0; i <= clicker.length; i++) {
-		clicker.item(i).classList.add('menu_active');
-	}
+let menuSub = document.querySelectorAll('.menu_sub');
+let arrMenuSub = Array.from(menuSub);
+
+
+for (let i = 0; i < arrMenuSub.length; i++) {
+  let subClick = arrMenuSub[i].closest('.menu__item');
+  subClick.onclick = function () {
+      menuSub.item(i).classList.toggle('menu_active');
+      return false;
+  }
 }
