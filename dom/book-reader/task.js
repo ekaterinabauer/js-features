bc547c84cdbf1bbccc0958aaa7da7458bd8c1810
Ekaterina Bauer <bauer-katerina@mail.ru>
2019-09-23 17:47:31 +0300

    Задача 3 - доработка

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -3,24 +3,22 @@ let book = document.getElementById('book');
 let arrFontSize = Array.from(fontSize);
 let activeSize = 1;
 
-for (i = 0; i < arrFontSize.length; i++) {
-	function fonts (event) {
+function fonts (event) {
 		event.preventDefault();
 		let indexOfSize = arrFontSize.indexOf(event.target);
 		arrFontSize[activeSize].classList.remove('font-size_active');
 		arrFontSize[indexOfSize].classList.add('font-size_active');
 		activeSize = indexOfSize;
 		let activeFont = document.querySelector('.font-size_active');
-		if (activeFont.classList.contains('font-size_small')) {
-			book.classList.remove('book_fs-big');
-			book.classList.add('book_fs-small');
-		} else if (activeFont.classList.contains('font-size_big')) {
-			book.classList.remove('book_fs-small');
+		book.classList.remove('book_fs-big');
+		book.classList.remove('book_fs-small');
+		if (activeFont.dataset.size == 'big') {
 			book.classList.add('book_fs-big');
-		} else {
-			book.classList.remove('book_fs-small');
-			book.classList.remove('book_fs-big');
+		} else if (activeFont.dataset.size == 'small') {
+			book.classList.add('book_fs-small');
 		}
 	}
+
+for (i = 0; i < arrFontSize.length; i++) {
 	arrFontSize[i].addEventListener('click', fonts);
 }
