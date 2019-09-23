40d129a22770a1a9e8f19f0f596548b2d69164c9
Ekaterina Bauer <bauer-katerina@mail.ru>
2019-09-23 18:07:51 +0300

    Задача 1 - доработка

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -14,6 +14,17 @@ let minutes = ("0"+ min).slice(-2);
 let time = `${hours}:${minutes}`;
 
 function sendMessage(event) {
+
+  let robotAnsvers = [
+    "Вы не купили ни одного товара, чтобы так с нами разговаривать!",
+    "Кто тут?",
+    "Где ваша совесть?",
+    "К сожалению, все операторы сейчас заняты. Не пишите нам больше",
+    "Добрый день! До свидания!",
+    "Мы ничего не будем вам продавать!"
+    ];
+
+  let robotAnsver = robotAnsvers[Math.floor(Math.random()*robotAnsvers.length)];
   
   if(event.charCode == 13 && input.value != ""){
     messages.innerHTML += 
@@ -26,10 +37,11 @@ function sendMessage(event) {
     messages.innerHTML += 
     `<div class="message">
     <div class="message__time">${time}</div>
-    <div class="message__text">Добрый день!</div>
+    <div class="message__text">${robotAnsver}</div>
     </div>`;
     
   }
+  
 }
 
 input.addEventListener("keypress", sendMessage);
